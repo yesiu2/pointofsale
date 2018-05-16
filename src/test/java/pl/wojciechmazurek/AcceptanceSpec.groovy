d9173cc24d2e795037436cf6a19e614b960a5914
yesiu2 <woytehu@gmail.com>
2018-05-16 22:12:54 +0200

    Turning clients requirements into happy path scenario in acceptance test

--- src/test/java/pl/wojciechmazurek/AcceptanceSpec.groovy
+++ src/test/java/pl/wojciechmazurek/AcceptanceSpec.groovy
@@ -4,5 +4,20 @@ import spock.lang.Specification
 
 class AcceptanceSpec extends Specification {
 
+    def "single product sale"() {
 
+        given: 'product bar code is scanned'
+
+        when: 'product is found in database'
+
+        then: 'its name and price is printed on LCD display'
+
+        when: 'product is not found in database'
+
+        then: 'error message "Product not found" is printed on LCD display'
+
+        when: 'scanned code is empty'
+
+        then: 'error message "Invalid bar code is printed on LCD display'
+    }
 }
